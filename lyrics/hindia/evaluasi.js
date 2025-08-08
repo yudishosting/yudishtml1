86fa150a198164a28606ae6462876ac23d7e1d69
yudishosting <ytira4269@gmail.com>
2025-08-08 18:04:06 +0700

    auto update

--- lyrics/hindia/evaluasi.js
+++ lyrics/hindia/evaluasi.js
@@ -1,7 +1,7 @@
 var lyrics = [
   { time: 0.0, text: "Yang tak bisa terobati" },
-  { time: 4.8, text: "Biarlah mengering sendiri" },
-  { time: 10.2, text: "Menghias tubuh dan" },
+  { time: 0.68, text: "Biarlah mengering sendiri" },
+  { time: 0.16, text: "Menghias tubuh dan" },
   { time: 11.4, text: "Yang mengevaluasi ragamu" },
   { time: 18.6, text: "Hanya kau sendiri" },
   { time: 21.0, text: "Mereka tak mampu" },
