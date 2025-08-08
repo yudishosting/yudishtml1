40cdabfbe864cea5a21cfe94a719312de25020ac
yudishosting <ytira4269@gmail.com>
2025-08-08 13:05:30 +0700

    auto update

---
+++ lyrics/nina/feast.js
@@ -0,0 +1,16 @@
+var lyrics = [
+  { time: 0.21, text: "Saat engkau tertidur" },
+  { time: 0.26, text: "Aku pergi menghibur" },
+  { time: 0.31, text: "Beda kota, pisah raga, bukan masalahku" },
+  { time: 0.37, text: "Lihat wajahmu di layar, ku tetap bersyukur" },
+  { time: 0.42, text: "Saat engkau terjaga" },
+  { time: 0.45, text: "Aku 'kan ada di sana" },
+  { time: 0.48, text: "Sempatkan bermain dan bawakan cenderamata" },
+  { time: 0.52, text: "Satu sampai lima tahun, cepat tak terasa" },
+  { time: 0.59, text: "Segala hal kuupayakan untuk melindungi" },
+  { time: 1.04, text: "Tunggu aku kembali lagi esok pagi" },
+  { time: 1.14, text: "Tumbuh lebih baik, cari panggilanmu" },
+  { time: 1.25, text: "Jadi lebih baik dibanding diriku" },
+  { time: 1.28, text: "'Tuk sementara ini aku mengembara jauh" },
+  { time: 1.31, text: "Saat dewasa kau 'kan mengerti" }
+];
